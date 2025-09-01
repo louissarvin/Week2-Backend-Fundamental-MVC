64bd51ee8b8ddad7e7644984571c9ee88a404f63
louissarvin <louisbaxel@gmail.com>
2025-09-01 23:30:44 +0700

    Completing Async Task

---
+++ logic_nolep/asynchronus/async.js
@@ -0,0 +1,65 @@
+const users = [
+  { id: 1, username: "john_doe" },
+  { id: 2, username: "jane_smith" },
+  { id: 3, username: "alice" },
+];
+
+// Implementasi Callback
+function getUserDataCallback(userId, callback) {
+  //code
+  const user = users.find((user) => user.id == userId);
+
+  return callback(user);
+}
+
+// Implementasi Promise
+function getUserDataPromise(userId) {
+  //code
+  return new Promise((resolve, reject) => {
+    const user = users.find((user) => user.id == userId);
+    resolve(user);
+
+    if (err) {
+      reject(new Error("Something went wrong!"), err);
+    }
+  });
+}
+
+// Implementasi Async/Await
+async function getUserDataAsync(userId) {
+  //code
+  try {
+    const response = await new Promise((resolve) => {
+      const user = users.find((user) => user.id == userId);
+
+      resolve(user);
+    });
+
+    return response;
+  } catch (err) {
+    console.log("There's some error in async-await", err);
+  }
+}
+
+// Test Case Callback
+getUserDataCallback(1, (user) => {
+  console.log("Callback Result:", user);
+  // Output: Callback Result: { id: 1, username: 'john_doe' }
+});
+
+// Test Case Promise
+getUserDataPromise(2)
+  .then((user) => {
+    console.log("Promise Result:", user);
+    // Output: Promise Result: { id: 2, username: 'jane_smith' }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+
+// Test Case Async/Await
+(async () => {
+  const user = await getUserDataAsync(3);
+  console.log("Async/Await Result:", user);
+  // Output: Async/Await Result: { id: 3, username: 'alice' }
+})();
